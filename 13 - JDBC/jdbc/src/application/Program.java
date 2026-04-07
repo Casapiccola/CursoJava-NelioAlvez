40a0c61e10a8ee051cb1a27b3bc7debc4e8f12b5
Erik Casapiccola <contato.erikcasapiccola@gmail.com>
2026-04-07 20:00:13 -0300

    Adicionado atividade sobre banco de dados com java

---
+++ 13 - JDBC/jdbc/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select * from department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+            DB.closeConnection();
+        }
+    }
+}
